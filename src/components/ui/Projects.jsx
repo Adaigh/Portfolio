d14aea0fd8c5bc4b06803903d5060dc71361149a
Adaigh <adaigh@asu.edu>
2024-09-11 11:13:30 -0700

    Updating button interactions for GitHub pages

--- src/components/ui/Projects.jsx
+++ src/components/ui/Projects.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { useSiteContext } from "../../context/SiteContext"
 import ProjectFrame from "../utility/ProjectFrame"
 import SectionHeader from "../utility/SectionHeader"
@@ -7,6 +8,8 @@ const Projects = () => {
 
     const { project } = useSiteContext()
 
+    const [showNum, setShowNum] = useState(0)
+
     if (project) return (
 
         <div id="Projects" className="banner">
@@ -15,39 +18,19 @@ const Projects = () => {
                 title={project.header.title}
                 message={project.header.message} />
 
-            <div className="accordion mw-1300p mx-auto" id="projectAccordion">
+            <div className="mw-1300p mx-auto" id="projectAccordion">
                 {project.projects.map((entry, index) => {
-                    if (index == 0) {
-                        return (
-                            <div className="accordion-item" key={index}>
-                                <h2 className="accordion-header">
-                                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${index}`} aria-expanded="false" aria-controls={`#collapsezero`}>
-                                        {entry.title.text}
-                                    </button>
-                                </h2>
-                                <div id={`collapse${index}`} className="accordion-collapse collapse show" data-bs-parent="#projectAccordion">
-                                    <div className="accordion-body">
-                                        {entry.title.type == "text" && <>{entry.title.text.replace(/\(.+\)/, '')}</>}
-                                        {entry.title.type == "link" && <a target="_blank" href={entry.title.ref}>{entry.title.text.replace(/\(.+\)/, '')}</a>}
-                                        <ProjectFrame {...entry} />
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    }
                     return (
-                        <div className="accordion-item" key={index}>
-                            <h2 className="accordion-header">
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${index}`} aria-expanded="false" aria-controls={`#collapse${index}`}>
+                        <div key={index}>
+                            <div className="project-button-bg">
+                                <button className={showNum === index ? "accordion-button p-4 opacity-0" : "accordion-button p-4"} type="button" onClick={() => setShowNum(index)}>
                                     {entry.title.text}
                                 </button>
-                            </h2>
-                            <div id={`collapse${index}`} className="accordion-collapse collapse" data-bs-parent="#projectAccordion">
-                                <div className="accordion-body">
-                                    {entry.title.type == "text" && <>{entry.title.text.replace(/\(.+\)/, '')}</>}
-                                    {entry.title.type == "link" && <a target="_blank" href={entry.title.ref}>{entry.title.text.replace(/\(.+\)/, '')}</a>}
-                                    <ProjectFrame {...entry} />
-                                </div>
+                            </div>
+                            <div className={showNum === index ? "shrink active" : "shrink"}>
+                                {entry.title.type == "text" && <>{entry.title.text.replace(/\(.+\)/, '')}</>}
+                                {entry.title.type == "link" && <a target="_blank" href={entry.title.ref}>{entry.title.text.replace(/\(.+\)/, '')}</a>}
+                                <ProjectFrame {...entry} />
                             </div>
                         </div>
                     )
